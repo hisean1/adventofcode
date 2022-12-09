e265930e68fea4bb1e1572e3f1ad93784860275d
Sean Hill <hisean1@umbc.edu>
2022-12-08 19:45:25 -0500

    day 8 advent part 2

--- 2022/day08.cpp
+++ 2022/day08.cpp
@@ -13,7 +13,11 @@
 
 using namespace std;
 
-bool isVisible(const int row, const int column, const vector<vector<int>>& grid);
+enum Dir { n, s, e, w };
+
+bool isVisible(const int height, const vector<int>& treeline);
+int distanceToBlocker(const int height, const vector<int>& treeline);
+vector<int> getTreesInDirection(const int row, const int column, const vector<vector<int>>& grid, Dir d);
 
 int main()
 {
@@ -21,7 +25,7 @@ int main()
     input.open("day08.txt");
     string line;
     vector<vector<int>> treemap;
-    int sum = 0;
+    int sum = 0, bestview = 0, bestrow = 0, bestcolumn = 0;
     
     try
     {
@@ -43,17 +47,38 @@ int main()
         {
             for (int column = 0; column < treemap[row].size(); column++)
             {
-                if (isVisible(row, column, treemap))
+                // All edges are visible
+                if (row == 0 || row == treemap.size() - 1 || column == 0 || column == treemap[row].size() - 1)
                 {
                     sum++;
-                    cout << "1";
                 }
                 else
                 {
-                    cout << "0";
+                    bool visible = false;
+                    int viewscore = 1;
+
+                    for (int dir = n; dir <= w; dir++)
+                    {
+                        vector<int> v = getTreesInDirection(row, column, treemap, Dir(dir));
+
+                        if (isVisible(treemap[row][column], v))
+                        {
+                            visible = true;
+                        }
+                        viewscore *= distanceToBlocker(treemap[row][column], v);
+                    }
+                    if (viewscore > bestview)
+                    {
+                        bestview = viewscore;
+                        bestrow = row;
+                        bestcolumn = column;
+                    }
+                    if (visible)
+                    {
+                        sum++;
+                    }
                 }
             }
-            cout << endl;
         }
     }
     catch (exception e)
@@ -63,86 +88,90 @@ int main()
     }
 
     cout << "Sum: " << sum << endl;
+    cout << "Best View: (" << bestrow << "," << bestcolumn << "): " << bestview << endl;
     
     input.close();
     return 0;
 }
 
 /// <summary>
-/// Checks if a tree is visible from the outside
+/// Checks if a tree is visible from outside the forest
 /// </summary>
-/// <param name="row">Row of tree</param>
-/// <param name="column">Column of tree</param>
-/// <param name="grid">Grid of trees</param>
-/// <returns>True if tree is visible</returns>
-bool isVisible(const int row, const int column, const vector<vector<int>>& grid)
+/// <param name="height">Height of tree to check</param>
+/// <param name="treeline">Trees between target and viewer</param>
+/// <returns>Whether tree is visible</returns>
+bool isVisible(const int height, const vector<int>& treeline)
 {
-    // All edges are visible
-    if (row == 0 || row == grid.size() - 1 || column == 0 || column == grid[row].size() - 1)
+    for (int i = 0; i < treeline.size(); i++)
     {
-        return true;
-    }
-
-    // Check West visibility
-    bool visible = true;
-    for (int i = 0; i < row; i++)
-    {
-        if (grid[i][column] >= grid[row][column])
+        if (height <= treeline[i])
         {
-            visible = false;
-            break;  // Not visible from west
+            return false;   // Not visible
         }
     }
-    if (visible)
+
+    // Not blocked, must be visible
+    return true;
+}
+
+/// <summary>
+/// Calculates the distance to a view-blocking tree (if one exists)
+/// </summary>
+/// <param name="height">Height of tree to calculate</param>
+/// <param name="treeline">Trees in direction to calculate</param>
+/// <returns>Distance to view-blocking tree, or edge of forest otherwise</returns>
+int distanceToBlocker(const int height, const vector<int>& treeline)
+{
+    for (int i = 0; i < treeline.size(); i++)
     {
-        return true;
-    }
-    
-    // Check East visibility
-    visible = true;
-    for (int i = row + 1; i < grid.size(); i++)
-    {
-        if (grid[i][column] >= grid[row][column])
+        if (height <= treeline[i])
         {
-            visible = false;
-            break;  // Not visible from east
+            return i + 1;   // Blocked at i
         }
     }
-    if (visible)
-    {
-        return true;
-    }
+    // Unblocked to edge of forest
+    return treeline.size();
+}
 
-    // Check North visibility
-    visible = true;
-    for (int i = 0; i < column; i++)
+/// <summary>
+/// Gets the trees (in order) in a direction, from a grid coordinate
+/// </summary>
+/// <param name="row">Row of target tree</param>
+/// <param name="column">Column of target tree</param>
+/// <param name="grid">Grid of the forest heights</param>
+/// <param name="d">Direction to search</param>
+/// <returns>Vector containing tree heights</returns>
+vector<int> getTreesInDirection(const int row, const int column, const vector<vector<int>>& grid, Dir d)
+{
+    vector<int> v;
+
+    switch (d)
     {
-        if (grid[row][i] >= grid[row][column])
+    case n:
+        for (int i = row - 1; i >= 0; i--)
         {
-            visible = false;
-            break;  // Not visible from north
+            v.push_back(grid[i][column]);
         }
-    }
-    if (visible)
-    {
-        return true;
-    }
-
-    // Check South visibility
-    visible = true;
-    for (int i = column + 1; i < grid[row].size(); i++)
-    {
-        if (grid[row][i] >= grid[row][column])
+        break;
+    case s:
+        for (int i = row + 1; i < grid.size(); i++)
         {
-            visible = false;
-            break;  // Not visible from south
+            v.push_back(grid[i][column]);
         }
-    }
-    if (visible)
-    {
-        return true;
+        break;
+    case e:
+        for (int i = column + 1; i < grid.size(); i++)
+        {
+            v.push_back(grid[row][i]);
+        }
+        break;
+    case w:
+        for (int i = column - 1; i >= 0; i--)
+        {
+            v.push_back(grid[row][i]);
+        }
+        break;
     }
 
-    // Not visible
-    return false;
+    return v;
 }
